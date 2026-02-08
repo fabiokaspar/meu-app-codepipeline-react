67c02e490a3b0a9dc96c17c8c20a1b8f73bb0179
root <root@Fabio.localdomain>
2026-02-07 23:39:25 -0300

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="container">
+      <h1 className="title">
+        Curso Completo de Codepipeline
+      </h1>
+      <p className="subtitle">
+        Criado com React e Vite de forma super simples versão 2!
+      </p>
+    </div>
+  );
+}
+export default App
